cc2dd8667f3431e1df5a5a0793b615c69aaaa481
TurtleCat02 <bcspenguin1@gmail.com>
2024-09-27 16:33:34 -0400

    Added material ui dependency

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,20 +2,25 @@ import express from "express";
 import ViteExpress from "vite-express";
 import mongoose from "mongoose";
 
+// @ts-ignore
+import parseXLSX from "./parser/xlsxParser.js"
+
 const app = express();
 
 const userSchema = new mongoose.Schema({
-  user: {type: String, required: true, unique: true},
-  password: {String, required: true},
+    user: {type: String, required: true, unique: true},
+    password: {String, required: true},
 }, {versionKey: false});
 const User = new mongoose.Model("User", userSchema);
 
+app.post("/login", (req: express.Request, res: express.Response) => {
 
+});
 
 app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
+    res.send("Hello Vite + React + TypeScript!");
 });
 
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+    console.log("Server is listening on port 3000..."),
 );
