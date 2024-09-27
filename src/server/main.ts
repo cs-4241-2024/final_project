68af01c244fb235f43ac6195e98fde86382e25e1
TurtleCat02 <bcspenguin1@gmail.com>
2024-09-27 16:14:56 -0400

    Added env and dependencies

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,7 +1,16 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import mongoose from "mongoose";
 
 const app = express();
+
+const userSchema = new mongoose.Schema({
+  user: {type: String, required: true, unique: true},
+  password: {String, required: true},
+}, {versionKey: false});
+const User = new mongoose.Model("User", userSchema);
+
+
 
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
