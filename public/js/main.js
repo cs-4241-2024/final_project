a3506a55f56fe930b94cabbddcb6131228352325
SupermanBritt <57573715+SupermanBritt@users.noreply.github.com>
2024-10-01 20:32:12 -0400

    Added and styled a header
    Navbar inserts itself into the innerHTML of an element in pages via a function called onload.

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,27 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
-function iloveClicking(){
-  alert("I LOVE CLICKING")
+function iloveClicking() {
+	alert("I LOVE CLICKING")
 }
 
-// window.onload = function() {
-// }
+function loadNavBar() {
+
+}
+
+//Adds the navigation bar to the location of the navbar classon each page. To add the nav bar to a page just add the following HTML element <nav class="navbar"></nav>
+const nav = function () {
+	const navbar = document.querySelector('.navbar')
+	navbar.innerHTML = `
+	<table>
+		<th><a href="index.html">Home</a></th>
+		<th><a href="profile.html">Profile</a></th>
+		<th><a href="forum.html">Forum</a></th>
+		<th><a href="playlists.html">Playlists</a></th>
+		<th><a href="songs.html">Songs</a></th>
+		<th><a href="users.html">Users</a></th>
+	</table>`
+}
+
+window.onload = function () {
+	nav()
+}
