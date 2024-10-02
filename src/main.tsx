324075619f3579dd716c33002d02d5e9f93a5c32
Eddie Stump <ejstump13@gmail.com>
2024-10-01 20:30:44 -0400

    restoring main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import CreateEvent from "./createEvent.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <CreateEvent />
   </StrictMode>
 );
