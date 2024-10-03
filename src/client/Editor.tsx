db163174dfc69d2a891ae59b6c6a7686be828ac4
ColeGW <68122848+ColeGW@users.noreply.github.com>
2024-10-03 16:35:36 -0400

    Added File Upload Functionality

--- src/client/Editor.tsx
+++ src/client/Editor.tsx
@@ -5,6 +5,7 @@ import { IconButton } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { createTheme } from '@mui/material/styles';
 import TableComponent from './tableComponent';
+import FileUploadButton from './FileUploadButton';
 // index.js
 
 import { useState } from "react";
@@ -27,49 +28,39 @@ theme = createTheme(theme, {
 });
 
 
-
-
 function Editor() {
   const navigate = useNavigate();
   return (
     <div className="main">
-        <IconButton
-          className="back-button"
-          aria-label="back to home"
+      <IconButton
+        className="back-button"
+        aria-label="back to home"
+        onClick={() => {
+          console.log("back button clicked");
+          navigate('/Home');
+        }}
+      >
+        <ArrowBackIcon />
+      </IconButton>
+      <div className="editor-container">
+        <Button
+          variant="contained"
+          type="submit"
+          className="editor-button"
+          sx={{ backgroundColor: 'purple', color: 'white' }}
           onClick={() => {
-            console.log("back button clicked");
-            navigate('/Home');
+            console.log("Clear current data button clicked");
           }}
         >
-          <ArrowBackIcon />
-        </IconButton>
-        <div className="editor-container">
-                <Button
-                    variant="contained"
-                    type="submit"
-                    className="editor-button"
-                    sx={{ backgroundColor: 'purple', color: 'white' }}
-                    onClick={() => {
-                        console.log("Clear current data button clicked");
-                    }}
-                >
-                    Clear current data
-                </Button>
+          Clear current data
+        </Button>
 
-                <Button
-                    variant="contained"
-                    type="submit"
-                    className="editor-button"
-                    sx={{ backgroundColor: 'purple', color: 'white' }}
-                    onClick={() => {
-                        console.log("Import Workday Data button clicked");
-                    }}
-                >
-                    Import Workday Data
-                </Button>
-        </div>
-        <TableComponent name="Table 1" rowNum={5} />
+        <FileUploadButton />
+
+
       </div>
+      <TableComponent name="Table 1" rowNum={5} />
+    </div>
   );
 }
 
