db387ba3b5f23ddee8864088f024e2db0ed16811
twinz300 <wrlewis@wpi.edu>
2024-10-04 10:19:25 -0400

    Profile & Homepage Display (wip)

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,50 @@
-// FRONT-END (CLIENT) JAVASCRIPT HERE
+// // FRONT-END (CLIENT) JAVASCRIPT HERE
 
-function iloveClicking(){
-  alert("I LOVE CLICKING")
-}
+// document.addEventListener('DOMContentLoaded', () => {
+//   const loginFile = document.getElementById('loginPage');
+//   const usernameChosen = document.getElementById('username');
+//   const passwordChosen = document.getElementById('password');
 
-// window.onload = function() {
-// }
+//   loginFile.addEventListener('submit', async function (event) {
+//     event.preventDefault();
+
+//     const username = usernameChosen.value();
+//     const password = passwordChosen.value();
+
+//     if (username === '' || password === '') {
+
+//       alert("You left your username and/or password slot blank. Please try again!");
+//       return;
+//     }
+
+
+//     try {
+
+//       const response = await fetch('/login', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ username, password }),
+//       });
+
+
+//       const data = await response.json();
+//       if (data.success && response.ok) {
+//         alert("You have successfully logged in!")
+
+//         //INSERT CODE HERE LATER TO REDIRECT TO PROFILE HOME PAGE
+//       }
+//       else {
+//         alert("Login unsuccessful. Please try again.")
+//       }
+//     }
+//     catch {
+//       alert("ERROR! ERROR! Login unsuccessful! Please try again at a later time.")
+//     }
+
+
+//   })
+
+
+// })
