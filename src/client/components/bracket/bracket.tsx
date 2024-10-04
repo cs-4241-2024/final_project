1bb9d73bd5e11c85059dfbcf392b3e914cbfb23b
Jeremy Mulia Kurtz <kurtzjeremy126@gmail.com>
2024-10-03 22:42:27 -0400

    made BYES easier to see

--- src/client/components/bracket/bracket.tsx
+++ src/client/components/bracket/bracket.tsx
@@ -1,8 +1,9 @@
 export default function Bracket(props: {name?: string, subtitle?: string, className?: string}) {
     return (<div className={
         (props.className ? " " + props.className : "") +
-            (props.name==="BYE" ? "bg-gray-700 text-white" : " ") +
-            " p-4 rounded-3xl bg-sky-50 text-cyan-900 my-2 border-2"}>
+            (props.name=="BYE" ? "bg-gray-700 text-white" : " bg-sky-50 text-cyan-900 ") +
+            (props.subtitle=="5A" ? " row-start-3" : "") +
+            " p-4 rounded-3xl my-2 border-2"}>
 
             <h1>{props.name ? props.name : "Not Determined"}</h1>
             {props.subtitle ? <h2 className={"text-xs"}>{props.subtitle}</h2> : ""}
