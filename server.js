ef881bb33e03f60a26a4950bdbf23ce8e873e7f3
SupermanBritt <57573715+SupermanBritt@users.noreply.github.com>
2024-10-04 08:21:23 -0400

    Revert "KenyonRoutesControllers"

--- server.js
+++ server.js
@@ -10,22 +10,11 @@ app.use(express.urlencoded({ extended: false })); //url parser
 app.use(express.json()) // parse data as json
 const port = 3000
 
-const userRoutes = require("./routes/userRoutes");
-const postRoutes = require("./routes/postRoutes");
-const playlistRoutes = require("./routes/playlistRoutes");
-const replyRoutes = require("./routes/replyRoutes");
-const songRoutes = require("./routes/songRoutes");
 
 app.get('/', (req, res) => {
     res.sendFile('/public/index.html', {root: __dirname})
 })
 
-// defining routes
-app.use('/api/users', userRoutes);
-app.use('/api/posts', postRoutes);
-app.use('/api/songs', songRoutes);
-app.use('/api/replies', replyRoutes);
-app.use('/api/playlists', playlistRoutes);
 
 // allow all files in public to be served
 app.use(express.static('public'))
