a98c8b2088dd614185695f8f8a6818315d9a2ece
Jeremy Mulia Kurtz <kurtzjeremy126@gmail.com>
2024-10-05 16:48:51 -0400

    game submission completed

--- src/client/components/form.tsx
+++ src/client/components/form.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import {Match} from "../../types/match";
+// import {Match} from "../../types/match"; old params: setMatches: Function, matches: Match[]
+import Input from "./form/input";
 
 
-export default function form(setMatches: Function, matches: Match[]): React.ReactElement {
+
+
+export default function form() {
     const add = async (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
 
         const input = {
             MatchType: document.querySelector('input[name="match-type"]:checked') as HTMLInputElement,
-            MatchFormat: document.querySelector('input[name="match-format"]:checked') as HTMLInputElement,
             Match: document.getElementById('match') as HTMLInputElement,
-            SchoolA: document.getElementById("schoolA") as HTMLInputElement,
-            SchoolB: document.getElementById("schoolB") as HTMLInputElement,
             PlayerA1: document.getElementById("playerA1") as HTMLInputElement,
             PlayerB1: document.getElementById("playerB1") as HTMLInputElement,
             PlayerA2: document.getElementById("playerA2") as HTMLInputElement,
@@ -23,22 +23,9 @@ export default function form(setMatches: Function, matches: Match[]): React.Reac
             Game3A: document.getElementById("game3A") as HTMLInputElement,
             Game3B: document.getElementById("game3B") as HTMLInputElement,
         };
-        // if(input.MatchType === null ||
-        //     input.MatchFormat === null ||
-        //     input.Match === null || input.Match.value === '' ||
-        //     input.SchoolA === null || input.SchoolA.value === '' ||
-        //     input.SchoolB === null || input.SchoolB.value === '' ||
-        //     input.PlayerA1 === null || input.PlayerA1.value === '' ||
-        //     input.PlayerB1 === null || input.PlayerB1.value === '') {
-        //     alert('Please fill out all required fields')
-        //     return
-        // }
         const json = {
             MatchType: input.MatchType.value,
-            MatchFormat: input.MatchFormat.value,
             Match: input.Match.value,
-            SchoolA: input.SchoolA.value,
-            SchoolB: input.SchoolB.value,
             PlayerA1: input.PlayerA1.value,
             PlayerB1: input.PlayerB1.value,
             PlayerA2: input.PlayerA2.value,
@@ -59,47 +46,44 @@ export default function form(setMatches: Function, matches: Match[]): React.Reac
             body
         })
         const data = await response.json()
-        console.log(data)
-        setMatches([...matches, data]);
-        console.log(matches)
+        // console.log(data)
+        // setMatches([...matches, data]);
+        // console.log(matches)
     }
 
 
-    return <form>
-        <h2 className="is-size-4 is-flex is-justify-content-start is-align-items-start has-text-weight-semibold">Match
-            Formatting:</h2>
-        <div className="is-flex is-flex-direction-row is-justify-content-space-between ">
-            <div className="is-flex is-flex-direction-column">
-                <h3>Match Type:</h3>
-                <div className="flex-horizontal">
+    return <form className={""}>
+        <h2 className="text-2xl flex justify-start items-start font-bold">Match Formatting:</h2>
+        <div className="flex flex-row gap-5">
+            <div className="flex flex-col">
+                <h3 className={"font-semibold"}>Match Type:</h3>
+                <div className="flex">
                     <input type="radio" id="round-robin" name="match-type" value="Round Robin" required/>
                     <label htmlFor="round-robin">Round Robin</label>
                 </div>
-                <div className="is-flex is-flex-direction-row">
+                <div className="flex flex-row">
                     <input type="radio" id="elimination" name="match-type" value="Elimination" required/>
                     <label htmlFor="elimination">Elimination</label>
                 </div>
             </div>
 
+            {/*<div className="flex flex-col">*/}
+            {/*    <h3 className="pb-1">Match Format:</h3>*/}
+            {/*    <div className="flex flex-row">*/}
+            {/*        <input type="radio" id="singles" name="match-format" value="singles" required/>*/}
+            {/*        <label htmlFor="singles">Singles</label>*/}
+            {/*    </div>*/}
+            {/*    <div className="flex flex-row">*/}
+            {/*        <input type="radio" id="doubles" name="match-format" value="doubles" required/>*/}
+            {/*        <label htmlFor="doubles">Doubles</label>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
 
-            <div className="is-flex is-flex-direction-column">
-                <h3 className="pb-1">Match Format:</h3>
-                <div className="is-flex is-flex-direction-row">
-                    <input type="radio" id="singles" name="match-format" value="singles" required/>
-                    <label htmlFor="singles">Singles</label>
-                </div>
-                <div className="is-flex is-flex-direction-row">
-                    <input type="radio" id="doubles" name="match-format" value="doubles" required/>
-                    <label htmlFor="doubles">Doubles</label>
-                </div>
-            </div>
-
-
-            <div className="is-flex is-justify-content-start is-flex-direction-column">
-                <label className="pb-1">Match:</label>
-                <div className="select">
-                    <select aria-label="What Match Group is this for i.e. Singles 3" id="match" name="match"
-                            required>
+            <div className="flex flex-col justify-start">
+                <label className="pb-1 font-semibold">Match:</label>
+                <div className="relative">
+                    <select aria-label="What Match Group is this for i.e. Singles 3" id="match" name="match" required
+                            className="block w-full bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                         <option value="">Choose Match Type</option>
                         <option value="Singles 1">Singles 1</option>
                         <option value="Singles 2">Singles 2</option>
@@ -112,83 +96,44 @@ export default function form(setMatches: Function, matches: Match[]): React.Reac
                 </div>
             </div>
         </div>
-        <datalist id="schools">
-            <option value="SAN">Sandburg</option>
-            <option value="AND">Andrew</option>
-            <option value="NT">New Trier</option>
-            <option value="DGN">Downers Grove North</option>
-            <option value="FR">Fremd</option>
-            <option value="HER">Hersey</option>
-            <option value="HS">Hinsdale South</option>
-            <option value="LT">Lockport</option>
-            <option value="NN">Naperville North</option>
-            <option value="DF">Deerfield</option>
-            <option value="WY">Whitney Young</option>
-            <option value="YK">York</option>
-        </datalist>
 
         <div className="pt-3">
+            <h3 className="font-semibold text-2xl flex justify-start">Scores:</h3>
+            <div className={"grid grid-cols-3"}>
+                <p>Player 1</p>
+                <Input label="Player 1 for School A" id="playerA1" name="playerA1" placeholder="NT1" required/>
+                <Input label="Player 1 for School B" id="playerB1" name="playerB1" placeholder="AND2" required/>
 
-            <table>
-                <tr>
-                    <td><h3 className="has-text-weight-semibold is-size-4 is-flex is-justify-content-start">Scores:</h3>
-                    </td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td className="p-2">Schools</td>
-                    <td className="school-a">
-                        <input aria-label="Input for School A" className="input" list="schools" id="schoolA"
-                               name="schoolA" placeholder="Select School" required/>
-                    </td>
-                    <td className="school-b">
-                        <input aria-label="Input for School B" className="input" list="schools" id="schoolB"
-                               name="schoolB" placeholder="Select School" required/>
-                    </td>
-                </tr>
+                <p>Player 2</p>
+                <Input label="Player 2 for School A" id="playerA2" name="playerA2" placeholder="NT2"/>
+                <Input label="Player 2 for School B" id="playerB2" name="playerB2" placeholder="AND4"/>
 
-                <tr>
-                    <td className="p-2">PLAYER 1</td>
-                    <td><input aria-label="Player 1 for School A" className="input" type="text" id="playerA1"
-                               name="playerA1" placeholder="NT1" required/></td>
-                    <td><input aria-label="Player 1 for School B" className="input" type="text" id="playerB1"
-                               name="playerB1" placeholder="AND2" required/></td>
-                </tr>
-                <tr>
-                    <td className="p-2">PLAYER 2</td>
-                    <td><input aria-label="Player 2 for School A" className="input" type="text" id="playerA2"
-                               name="playerA2" placeholder="NT2"/></td>
-                    <td><input aria-label="Player 2 for School B" className="input" type="text" id="playerB2"
-                               name="playerB2" placeholder="AND4"/></td>
-                </tr>
+                <p>Game 1</p>
+                <Input label="Game 1 Score for School A" id="game1A" name="game1A" type="number" placeholder="0"
+                       required min={0}/>
+                <Input label="Game 1 Score for School B" id="game1B" name="game1B" type="number" placeholder="0"
+                       required min={0}/>
 
-                <tr>
-                    <td className="p-2">GAME 1</td>
-                    <td><input aria-label="Game 1 Score for School A" id="game1A" className="input" type="number"
-                               name="game1A" min={0} placeholder={"0"} required/></td>
-                    <td><input aria-label="Game 1 Score for School B" id="game1B" className="input" type="number"
-                               name="game1B" min={0} placeholder={"0"} required/></td>
-                </tr>
-                <tr>
-                    <td className="p-2">GAME 2</td>
-                    <td><input aria-label="Game 2 Score for School A" id="game2A" className="input" type="number"
-                               name="game2A" min={0} placeholder={"0"} required/></td>
-                    <td><input aria-label="Game 2 Score for School B" id="game2B" className="input" type="number"
-                               name="game2B" min={0} placeholder={"0"} required/></td>
-                </tr>
-                <tr>
-                    <td className="p-2">GAME 3</td>
-                    <td><input aria-label="Game 3 Score for School A" id="game3A" className="input" type="number"
-                               name="game3A" min={0} placeholder={"0"}/></td>
-                    <td><input aria-label="Game 3 Score for School B" id="game3B" className="input" type="number"
-                               name="game3B" min={0} placeholder={"0"}/></td>
-                </tr>
-            </table>
+                <p>Game 2</p>
+                <Input label="Game 2 Score for School A" id="game2A" name="game2A" type="number" placeholder="0"
+                       required min={0}/>
+                <Input label="Game 2 Score for School B" id="game2B" name="game2B" type="number" placeholder="0"
+                       required min={0}/>
+
+                <p>Game 3</p>
+                <Input label="Game 3 Score for School A" id="game3A" name="game3A" type="number" placeholder="0"
+                       min={0}/>
+                <Input label="Game 3 Score for School B" id="game3B" name="game3B" type="number" placeholder="0"
+                       min={0}/>
+            </div>
         </div>
-        <div className="mt-2 submit-clear flex-horizontal is-justify-content-end is-align-items-end">
-            <button type="button" id="add" className="button is-link" value="Submit" onClick={add}>Submit</button>
-            <button type="reset" className="ml-2 button is-link is-light">Clear</button>
+        <div className="mt-2 flex justify-end items-end">
+            <button type="button" id="add" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700"
+                    value="Submit" onClick={add}>Submit
+            </button>
+            <button type="reset"
+                    className="ml-2 bg-white text-black border border-blue-500 rounded-md px-4 py-2 hover:bg-gray-400">Clear
+            </button>
         </div>
     </form>
 }
