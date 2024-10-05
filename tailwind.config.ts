9396c1be87abae3675eb3147e14debe427cd415f
Jeremy Mulia Kurtz <kurtzjeremy126@gmail.com>
2024-10-05 16:54:25 -0400

    Add two colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'TrevBlue': '#214479',
+        'TrevGreen': '#1E6837',
+      }
+    },
   },
   plugins: [],
 }
