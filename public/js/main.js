6a1bbf90944a49918548e3bc7a688d6215a0b27c
SupermanBritt <57573715+SupermanBritt@users.noreply.github.com>
2024-10-05 16:26:38 -0400

    CSS changes

--- public/js/main.js
+++ public/js/main.js
@@ -9,13 +9,13 @@ function iloveClicking() {
 const nav = function () {
 	const navbar = document.querySelector('.navbar')
 	navbar.innerHTML = `
-	<table>
-		<th><a class=navbarLink href="index.html">Home</a></th>
-		<th><a class=navbarLink href="profile.html">Profile</a></th>
-		<th><a class=navbarLink href="forum.html">Forum</a></th>
-		<th><a class=navbarLink href="playlists.html">Playlists</a></th>
-		<th><a class=navbarLink href="songs.html">Songs</a></th>
-		<th><a class=navbarLink href="users.html">Users</a></th>
+	<table class=navbar>
+		<th class=navbarItem><a class=navbarLink href="index.html">Home</a></th>
+		<th class=navbarItem><a class=navbarLink href="profile.html">Profile</a></th>
+		<th class=navbarItem><a class=navbarLink href="forum.html">Forum</a></th>
+		<th class=navbarItem><a class=navbarLink href="playlists.html">Playlists</a></th>
+		<th class=navbarItem><a class=navbarLink href="songs.html">Songs</a></th>
+		<th class=navbarItem><a class=navbarLink href="users.html">Users</a></th>
 	</table>`
 }
 
