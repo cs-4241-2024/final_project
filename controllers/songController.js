9bc337ea838e68a14c6902defd7f1e948cf14880
Kenyon Coleman <kenyon.cole.man15@gmail.com>
2024-10-05 17:00:32 -0400

    add song

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,19 +1,48 @@
-export function addSong(){
-    return "TODO"
+import pkg from 'mongodb';
+const { MongoClient, ObjectID } = pkg;
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const uri = `mongodb+srv://webwaresongsite:mongodb!@songsite.g4glr.mongodb.net/?retryWrites=true&w=majority&appName=SongSite`;
+const client = new MongoClient(uri);
+
+export const addSong = async (req, res) => {
+    try {
+        await client.connect();
+        const db = client.db('SongWebsite');
+
+        const newSong = {
+            name: req.body.name,
+            artist: req.body.artist,
+            album: req.body.album,
+            duration: req.body.duration,
+            genre: req.body.genre,
+            releaseDate: req.body.releaseDate,
+            createdBy: req.body.createdBy,
+            createdOn: new Date()
+        };
+
+        const result = await db.collection('songs').insertOne(newSong);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({error: 'Error creating song'});
+        console.error('Error creating song:', error);
+    } finally {
+        await client.close();
+    }
 }
 
-export function getSongs(){
-    return "TODO"
-}
 
-export function getSongByID(){
-    return "TODO"
-}
 
-export function updateSong(){
-    return "TODO"
-}
 
-export function deleteSong(){
-    return "TODO"
-}
+
+
+
+
+
+
+
+
+
