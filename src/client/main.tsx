7cd3d1ac3b3883dc9eab20f8833ea00b33101be6
TurtleCat02 <bcspenguin1@gmail.com>
2024-10-06 20:38:42 -0400

    Started refactor

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,27 +1,44 @@
-import "./css/index.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./css/Root.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// import App from "./routes/App";
+import Root from "./routes/Root";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
 import About from "./routes/About";
 import Editor from "./routes/Editor";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/Home" element={<Home />} />
-        {/*<Route path="/App" element={<App />} />*/}
-        <Route path="/Login" element={<Login />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Editor" element={<Editor />} />
-      </Routes>
-    </Router>
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        loader: async () => {
+            const res = await fetch("/checkLogin", {
+                method: "POST",
+            });
 
-  </React.StrictMode>,
+            if (res.status === 200) {
+
+            }
+
+        }
+    }
+])
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    {/*<Router>*/}
+    {/*  <Routes>*/}
+    {/*    <Route path="/" element={<Login />} />*/}
+    {/*    <Route path="/Home" element={<Home />} />*/}
+    {/*    /!*<Route path="/Root" element={<Root />} />*!/*/}
+    {/*    <Route path="/Login" element={<Login />} />*/}
+    {/*    <Route path="/About" element={<About />} />*/}
+    {/*    <Route path="/Editor" element={<Editor />} />*/}
+    {/*  </Routes>*/}
+    {/*</Router>*/}
+    </React.StrictMode>
 );
