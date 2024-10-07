9346e1f51a85c4f2304c23c84595859918e45976
SupermanBritt <57573715+SupermanBritt@users.noreply.github.com>
2024-10-07 16:29:05 -0400

    Search Results page title dynamically changes

--- public/js/searchResults.js
+++ public/js/searchResults.js
@@ -2,10 +2,10 @@ import { nav } from "./main.js";
 import { makeURLWithParams, getParam } from "./urlHelpers.js";
 
 function setup() {
-
+	const query = getParam(window.location.href, "query");
+	const seachResultsPageTitle = document.getElementById("searchPageTitle");
+	seachResultsPageTitle.textContent = "Search Results for " + query;
 }
-
-
 
 window.onload = function () {
 	nav();
