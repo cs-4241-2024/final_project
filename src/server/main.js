5908553d322214939fb973ac663fd8933f4fc7ad
SHADOW12021 <gsoccer2003@gmail.com>
2024-10-07 12:54:01 -0400

    Working Server

--- src/server/main.js
+++ src/server/main.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || 'your secret option here',
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false, sameSite: 'lax' }
