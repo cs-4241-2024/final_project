0371acbed7fe499c718bc0e9d883a9a6667df0f3
Liswerd <dragonweirdo4714@gmail.com>
2024-10-07 19:32:45 -0400

    finished song page,

---
+++ public/js/songs.js
@@ -0,0 +1,47 @@
+import { nav } from "./main.js";
+
+export async function getSong() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    // ID of song in database (string)
+    //let songID = "66fad67d7c666b549aef4189";http://localhost:3000/songs.html/?song=66fad67d7c666b549aef4189
+    let songID = urlParams.get('song');
+    console.log(queryString);
+    console.log(urlParams["song"]);
+
+    // Request to get a song by ID
+    const response = await fetch(`/api/songs/${songID}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    // Contains song object if found
+    let song = await response.json();
+    console.log(song);
+
+
+    let title = document.getElementById("SongTitle");
+    title.textContent = song.name;
+
+    let artist = document.getElementById("ARTIST");
+    artist.textContent = "Artist: " + song.artist;
+
+    let album = document.getElementById("ALBUM");
+    album.textContent = "Album: " + song.album;
+
+    let duration = document.getElementById("DUR");
+    duration.textContent = "Duration: " + song.duration;
+
+    let genre = document.getElementById("GENRE");
+    genre.textContent = "Genre: " + song.genre;
+
+    let date = document.getElementById("DATE");
+    date.textContent = "Date created: " + song.releaseDate;
+}
+
+
+function getSongName() {
+
+}
+
+window.onload = function () { nav(); getSong().then() }
