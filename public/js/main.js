6289dcaebc3911c071110f16e6f699ddca79d9ec
SupermanBritt <57573715+SupermanBritt@users.noreply.github.com>
2024-10-07 15:01:14 -0400

    Search button works
    Search button sends the user to the search-results page with a parameter of the query that they entered.

--- public/js/main.js
+++ public/js/main.js
@@ -15,6 +15,38 @@ const nav = function () {
 	</table>`
 }
 
+//Sends the user to the search-results page with parameters in the url
+function search() {
+	const query = document.getElementById("searchInput");
+	if (query.value != "") {
+		const urlWithParams = makeURLWithParams("search-results", "query", query.value);
+		console.log(urlWithParams);
+		const paramValue = getParam(urlWithParams, "query");
+		console.log(paramValue);
+		location.assign(urlWithParams);
+	}
+	else {
+		alert("Please enter something to search for!");
+	}
+}
+
 window.onload = function () {
 	nav()
+}
+
+//From urlHelpers.js
+// ---------------------------------------------------------------------------------------------
+//Returns a string of a url for the page you want to go to with the parameters it should have
+function makeURLWithParams(htmlFileName, paramName, paramValue) {
+	let urlString = window.location.href;
+	urlString = urlString.slice(0, -(window.location.pathname.length));
+	urlString += "/" + htmlFileName + ".html?" + paramName + "=" + paramValue;
+	return urlString;
+}
+
+//When you are on the page with a url with parameters you would get that url by passing in window.location.href as urlWithParams
+function getParam(urlWithParams, paramName) {
+	let url = new URL(urlWithParams);
+	let params = new URLSearchParams(url.search);
+	return params.get(paramName);
 }
