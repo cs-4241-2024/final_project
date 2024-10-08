ddd11543d6b49832d7edf8f1b3cd005b53e306f6
SupermanBritt <57573715+SupermanBritt@users.noreply.github.com>
2024-10-08 13:02:10 -0400

    Update urlHelpers.js
    Broke from parameters. Bug is fixed.

--- public/js/urlHelpers.js
+++ public/js/urlHelpers.js
@@ -1,13 +1,14 @@
 //Returns a string of a url for the page you want to go to with the parameters it should have
-function makeURLWithParams(htmlFileName, paramName, paramValue) {
-	let urlString = window.location.href;
+export function makeURLWithParams(htmlFileName, paramName, paramValue) {
+	// let urlString = window.location.href;
+	let urlString = window.location.href.split('?')[0]
 	urlString = urlString.slice(0, -(window.location.pathname.length));
 	urlString += "/" + htmlFileName + ".html?" + paramName + "=" + paramValue;
 	return urlString;
 }
 
 //When you are on the page with a url with parameters you would get that url by passing in window.location.href as urlWithParams
-function getParam(urlWithParams, paramName) {
+export function getParam(urlWithParams, paramName) {
 	let url = new URL(urlWithParams);
 	let params = new URLSearchParams(url.search);
 	return params.get(paramName);
