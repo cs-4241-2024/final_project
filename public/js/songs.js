b8ede6bde5601ed796d4024d83d2a0c037b17633
SupermanBritt <57573715+SupermanBritt@users.noreply.github.com>
2024-10-09 11:11:00 -0400

    Adds song to favorites list
    TODO: Check for duplicates.

--- public/js/songs.js
+++ public/js/songs.js
@@ -1,11 +1,14 @@
 import { nav } from "./main.js";
 
+let songId;
+
 async function getSong() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     // ID of song in database (string)
     //let songID = "66fad67d7c666b549aef4189";http://localhost:3000/songs.html/?id=66fad67d7c666b549aef4189
     let songID = urlParams.get('id');
+    songId = songID;
     console.log(queryString);
     console.log(urlParams["song"]);
 
@@ -36,10 +39,27 @@ async function getSong() {
 
     let releaseDate = document.getElementById("releaseDate");
     releaseDate.textContent = "Release Date: " + song.releaseDate;
+
+    const addToFavoritesButton = document.getElementById("addToFavoritesButton");
+    addToFavoritesButton.onclick = addSongToFavorites;
 }
 
-function getSongName() {
+const addSongToFavorites = async function (songId) {
+    const responseUserId = await fetch('/api/users/git/dbID', {
+        method: 'get'
+    })
+    //contains the current logged in user’s database ID
+    let userID = await responseUserId.text()
 
+    console.log(userID);
+
+    //Add the song to the usser's favorite songs
+    const response = await fetch(`/api/users/favorites/${songId}`, {
+        method: 'POST'
+    })
+    let result = await response.text();
+
+    alert("Song successfully added to your favorites!");
 }
 
 window.onload = function () {
