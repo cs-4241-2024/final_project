354ea28aa1fdf3aa183d46d850593ccef2c5d415
Kenyon Coleman <kenyon.cole.man15@gmail.com>
2024-10-09 11:57:25 -0400

    fixes

--- public/js/addSong.js
+++ public/js/addSong.js
@@ -1,53 +1,57 @@
-export async function getUserID(){
-    const response = await fetch('/api/users/git/dbID', {
-        method: 'GET'
-    });
+import {makeURLWithParams} from "./urlHelpers.js";
 
-    if(response.ok){
-        return await response.text();
-    } else {
-        console.error("Failed to retrieve user ID");
-        return null;
-    }
-}
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("addSongForm").addEventListener("submit", async function (event) {
+        event.preventDefault();
 
-const userId = await getUserID();
-
-document.getElementById("addSongForm").addEventListener("submit", async function (event) {
-    event.preventDefault();
-
-    const name = document.getElementById("name").value;
-    const artist = document.getElementById("artist").value;
-    const album = document.getElementById("album").value;
-    const duration = document.getElementById("duration").value;
-    const genre = document.getElementById("genre").value;
-    const releaseDate = document.getElementById("releaseDate").value;
-
-    const newSong = {
-        name: name,
-        artist: artist,
-        album: album,
-        duration: duration,
-        genre: genre,
-        releaseDate: releaseDate,
-        createdBy: userId,
-        createdOn: new Date()
-    }
-
-    try {
-        const response = await fetch('/api/songs', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(newSong),
-        })
-
-        if (response.ok) {
-            const result = await response.json();
-            document.getElementById("responseMessage").textContent = "Song added!"
-        } else {
-            document.getElementById("responseMessage").textContent = "Failed to add song"
+        let userID = null;
+        try {
+            const userResponse = await fetch('/api/users/git/dbID', {method: 'GET'});
+            if (userResponse.ok) {
+                userID = await userResponse.text();
+            } else {
+                console.log('Failed to get user ID');
+                return;
+            }
+        } catch (error) {
+            console.error("Error getting user ID:", error);
         }
-    } catch (error) {
-        console.log(error);
-    }
+
+        const name = document.getElementById("name").value;
+        const artist = document.getElementById("artist").value;
+        const album = document.getElementById("album").value;
+        const duration = document.getElementById("duration").value;
+        const genre = document.getElementById("genre").value;
+        const releaseDate = document.getElementById("releaseDate").value;
+
+        const newSong = {
+            name: name,
+            artist: artist,
+            album: album,
+            duration: duration,
+            genre: genre,
+            releaseDate: releaseDate,
+            createdBy: userID,
+            createdOn: new Date()
+        }
+
+
+
+        try {
+            const response = await fetch('/api/songs', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(newSong),
+            })
+
+            if (response.ok) {
+                const result = await response.json()
+                const songId = result.insertedId;
+                window.location.href = makeURLWithParams("songs", "id", songId);
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    });
 });
