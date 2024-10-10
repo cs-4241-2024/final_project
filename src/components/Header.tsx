a637ea830bf4894d2b0cd43aa44848b462423612
harbar20 <harbar2021@gmail.com>
2024-10-09 22:50:46 -0400

    added header to sign in page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,20 +11,18 @@ export default function Header() {
 
     return (
         <header>
-            <SignedOut>
-                <div className="py-3 px-6 text-lg">
-                    <SignInButton />
-                </div>
-            </SignedOut>
-            <SignedIn>
-                <div className="flex flex-row py-3 px-6 bg-pink-200 border-b border-red-500">
-                    <p className="text-xl">DayTime</p>
-                    <div className="flex flex-row items-center w-full justify-end gap-2">
+            <div className="flex flex-row py-3 px-6 bg-pink-200 border-b border-red-500">
+                <p className="text-xl">DayTime</p>
+                <div className="flex flex-row items-center w-full justify-end gap-2">
+                    <SignedOut>
+                        <SignInButton />
+                    </SignedOut>
+                    <SignedIn>
                         <p>Welcome {user?.firstName}</p>
                         <UserButton />
-                    </div>
+                    </SignedIn>
                 </div>
-            </SignedIn>
+            </div>
         </header>
     );
 }
